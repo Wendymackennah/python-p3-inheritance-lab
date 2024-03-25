9bc357d84680aab504c188a3c5896e43d9c23d71
wendymackennah <wendymackennah5@gmail.com>
2024-03-25 23:35:34 +0300

    completed

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,42 @@
 
 from user import User
 
+
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, new_information):
+        self.knowledge.append(new_information)
+
+
+# Run the tests
+class TestStudent:
+    '''Class "Student" in student.py'''
+
+    def test_is_subclass(self):
+        '''is a subclass of "User".'''
+        assert (issubclass(Student, User))
+
+    def test_initializes_with_names(self):
+        '''initializes with first and last name.'''
+        my_student = Student("My", "Student")
+        assert ((my_student.first_name, my_student.last_name) == ("My", "Student"))
+
+    def test_initializes_with_knowledge(self):
+        '''initializes with empty list attribute "knowledge".'''
+        my_student = Student("My", "Student")
+        assert (hasattr(my_student, "knowledge"))
+
+    def test_can_learn(self):
+        '''learns from a string and adds to self.knowledge.'''
+        my_student = Student("My", "Student")
+        my_student.learn("New information")
+        assert ("New information" in my_student.knowledge)
--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,42 @@ from user import User
 
 import random
 
+
+
+class User:
+    pass  # Assuming User class is defined elsewhere
+
+
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = ["Math", "Science", "Literature"]  # Example knowledge list
 
     def teach(self):
-        pass
+        random_index = random.randint(0, len(self.knowledge) - 1)
+        return self.knowledge[random_index]
+
+
+# Run the tests
+my_teacher = Teacher("My", "Teacher")
+
+class TestTeacher:
+    '''Class "Teacher" in teacher.py'''
+
+    def test_is_subclass(self):
+       '''is a subclass of "User".'''
+    assert (User in Teacher.__bases__)
+
+    def test_initializes_with_names(self):
+        '''initializes with first and last name.'''
+        assert ((my_teacher.first_name, my_teacher.last_name) == ("My", "Teacher"))
+
+    def test_has_attribute_knowledge(self):
+        '''has an attribute called "knowledge", a list with len > 0.'''
+        assert (isinstance(my_teacher.knowledge, list) and len(my_teacher.knowledge) > 0)
+
+    def test_can_teach(self):
+        '''teaches from list of knowledge.'''
+        my_teacher = Teacher("My", "Teacher")
+        assert (my_teacher.teach() in my_teacher.knowledge)
